d4d96a559ee3257edfa4284f9916c78d07e84522
banboy38 <nabovtl@gmail.com>
2020-11-10 22:35:42 +0530

    v3.1.1 bugfix

--- Blogs/blogaccept.php
+++ Blogs/blogaccept.php
@@ -2,8 +2,12 @@
     include "connection.php";
     
     session_start();
+
+    $maal = $_GET['blog'];
+
+    $maal = str_replace("'","&#39;",$maal);
         
-    mysqli_query($conn, "insert into content(user,email,blog,heading) values('{$_SESSION['user']}','{$_SESSION['email']}','{$_GET['blog']}','{$_GET['bheading']}');");
+    mysqli_query($conn, "insert into content(user,email,blog,heading) values('{$_SESSION['user']}','{$_SESSION['email']}','{$maal}','{$_GET['bheading']}');");
 
     header("Location: /view_blogs.php");
 
