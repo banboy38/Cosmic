bc1575d8bc7942d90e51e75530063df57d3f83c8
banboy38 <nabovtl@gmail.com>
2020-11-10 17:00:14 +0530

    vLIVE

--- Blogs/connection.php
+++ Blogs/connection.php
@@ -1,9 +1,9 @@
 <?php  
 
-    $servername = "localhost";  
-    $username = "root"; 
-    $password = "4761";  //put your own username and password
+    $servername = "sql109.epizy.com";  
+    $username = "epiz_27162868"; 
+    $password = "MiEB2YWS5Y";  //put your own username and password
     $conn = mysqli_connect ($servername , $username , $password) or die("unable to connect to host");  
-    $sql = mysqli_select_db ($conn, 'cosmic') or die("unable to connect to database"); //change database name to your convenience
+    $sql = mysqli_select_db ($conn, 'epiz_27162868_cosmic') or die("unable to connect to database"); //change database name to your convenience
 
 ?>
